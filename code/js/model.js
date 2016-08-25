899b731e21a905625e0c0d074a18ea94f1ef2f1b
Chris Ertel <chris@kedagital.com>
2016-08-25 13:19:45 -0500

    Cleanup OBJ parser.

--- code/js/model.js
+++ code/js/model.js
@@ -1,79 +1,61 @@
-function Model() {
-  this._verts = new Float32Array();
-  this._normals = new Float32Array();
-  this._uvs = new Float32Array();
-  this._faces = new Int32Array(); // in form v0,uv0,n0,v1,uv1,n1, etc.
+function Model(verts, normals, uvs, faces) {
+  this._verts = new Float32Array( verts || [] );
+  this._normals = new Float32Array( normals || [] );
+  this._uvs = new Float32Array( uvs || []) ;
+  this._faces = faces || [];
 }
 
 Model.prototype.toTriArray = function() {
-  // this assumes face indices are trilists and not strips
-  var faceCount = this._faces.length / 9;
-  var out = this._faces.reduce( function( acc, el, faceIndex) {
-    // unpack the face indexes into an array of 3-tuples
+  return this._faces.reduce( function( acc, faceElementIndex, faceIndex) {
     var currFace = Math.floor(faceIndex / 9);
+    var currVert = (faceIndex - 9 * currFace) /3 ;
+
     if (faceIndex % 3 == 0) {
-      var currVert = (faceIndex - 9 * currFace) /3 ;
-      acc[currFace*9 + currVert*3 + 0] = this._verts[3*el + 0];
-      acc[currFace*9 + currVert*3 + 1] = this._verts[3*el + 1];
-      acc[currFace*9 + currVert*3 + 2] = this._verts[3*el + 2];
-    }  
+      acc[currFace*24 + currVert*8 + 0] = this._verts[3*faceElementIndex + 0];
+      acc[currFace*24 + currVert*8 + 1] = this._verts[3*faceElementIndex + 1];
+      acc[currFace*24 + currVert*8 + 2] = this._verts[3*faceElementIndex + 2];
+    } else  if (faceIndex % 3 == 1) {
+      acc[currFace*24 + currVert*8 + 3] = this._normals[3*faceElementIndex + 0];
+      acc[currFace*24 + currVert*8 + 4] = this._normals[3*faceElementIndex + 1];
+      acc[currFace*24 + currVert*8 + 5] = this._normals[3*faceElementIndex + 2];
+    } else if (faceIndex % 3 == 2) {
+      acc[currFace*24 + currVert*8 + 6] = this._uvs[2*faceElementIndex + 0];
+      acc[currFace*24 + currVert*8 + 7] = this._uvs[2*faceElementIndex + 1];
+    }
     return acc;
-  }.bind(this), new Float32Array( faceCount * 9));
-  
-  return out;
+  }.bind(this), new Float32Array( (this._faces.length / 3) * 8) );
 }
 
-Model.prototype.loadFromOBJString = function( src ) {
+Model.loadFromOBJString = function( src ) {
   // split source into trimmed lines with comments removed
   var processedLines = src.split('\n')
                           .map( function(s) { return s.replace(/#.*/,'').trim(); })
                           .filter( function(s) { return s.length !== 0; });
 
-  var parseState = {
-    verts: [],
-    uvs: [],
-    norms: [],
-    faces: []
-  };
-
   function decodeVertex( toks ) {
-    if (toks.length !== 4) {
-      throw new Error("Malformed vertex");
-    }
+    if (toks.length !== 4) { throw new Error("Malformed vertex"); }
     return toks.slice(1).map(Number.parseFloat);
   };
 
   function decodeNormal( toks ) {
-    if (toks.length !== 4) {
-      throw new Error("Malformed normal");
-    }
-    
+    if (toks.length !== 4) { throw new Error("Malformed normal"); }
     return toks.slice(1).map(Number.parseFloat);
   };
 
   function decodeUV( toks ) {
-    if (toks.length !== 3) {
-      throw new Error("Malformed UV");
-    }
+    if (toks.length !== 3) { throw new Error("Malformed UV"); }
     return toks.slice(1).map(Number.parseFloat);
   };
 
-  function decodeFaceToken(tok){
-
-    var indices = {
-      vert: 0,
-      uv: 0,
-      normal: 0
-    };
-
+  function decodeFaceToken(tok){    
     var kVertIndexOnly = /\d*/;
     var kVertAndUVOnly = /^(\d*)\/(\d*)$/;
     var kVertAndNormalOnly = /^(\d*)\/\/(\d*)$/;
     var kVertUVNormal = /^(\d*)\/(\d*)\/(\d*)$/;
-
-    var match;
-
+    
     /* We have a lot of (...-1) here because OBJ is 1s indexed */
+    var match;
+    var indices = { vert: 0, uv: 0, normal: 0 };
     switch( true ) {
       case (kVertIndexOnly.test(tok)):      indices.vert = Number.parseInt(tok,10) - 1;
                                             break;
@@ -92,25 +74,19 @@ Model.prototype.loadFromOBJString = function( src ) {
                                             break;
       default: break;
     }
-
     return indices;
   }
 
   function decodeFace( toks ) {
-    if (toks.length < 4) {
-      throw new Error("Malformed face");
-    }
-
-    if (toks.length > 4) {
-      throw new Error("Nontriangulated face (unsupported)");
-    }
+    if (toks.length < 4) { throw new Error("Malformed face"); }
+    if (toks.length > 4) { throw new Error("Nontriangulated face (unsupported)"); }
 
     return toks.slice(1)
             .map(decodeFaceToken)
             .reduce( function globIndices( out, indices ){
               out.push( indices.vert);
-              out.push( indices.uv);
               out.push( indices.normal);
+              out.push( indices.uv);              
               return out;
           },[]);
   }
@@ -118,29 +94,24 @@ Model.prototype.loadFromOBJString = function( src ) {
   var parsedState = processedLines.reduce( function (state, line) {
     var toks = line.split(/\s+/);
     try {
-      switch(true) {
-        case ('v' === toks[0]) :  state.verts = state.verts.concat(decodeVertex(toks)); break;
-        case ('vn' === toks[0]) : state.norms = state.verts.concat(decodeNormal(toks)); break;
-        case ('vt' === toks[0]) : state.uvs = state.uvs.concat(decodeUV(toks)); break;
-        case ('f' === toks[0]) :  state.faces = state.faces.concat(decodeFace(toks)); break;
+      switch(toks[0]) {
+        case 'v':  state.verts = state.verts.concat(decodeVertex(toks)); break;
+        case 'vn': state.norms = state.verts.concat(decodeNormal(toks)); break;
+        case 'vt': state.uvs = state.uvs.concat(decodeUV(toks)); break;
+        case 'f':  state.faces = state.faces.concat(decodeFace(toks)); break;
         default: throw new Error("Unrecognized line"); break;
       }      
     } catch( e ) {
       console.log("ERROR ", e.toString(), '\n>>>\t ', line);
     }
     return state;
-  }, parseState);
-
-  this._verts = Float32Array.from( parsedState.verts );
-  this._uvs = Float32Array.from( parsedState.uvs );
-  this._normals = Float32Array.from( parsedState.norms );
-  this._faces = Int32Array.from(parsedState.faces);
+  }, { verts: [], uvs: [], norms: [], faces: [] });
 
   console.log('Model has\n',
-    '\t', this._verts.length/3, ' vertices\n',
-    '\t', this._uvs.length/2, ' UVs\n',
-    '\t', this._normals.length/3, ' normals\n',
-    '\t', this._faces.length/9, ' faces'); /* divide by 9 because 3 per face, 3 coords for position/vertx/normal */
+    '\t', parsedState.verts.length/3, ' vertices\n',
+    '\t', parsedState.uvs.length/2, ' UVs\n',
+    '\t', parsedState.norms.length/3, ' normals\n',
+    '\t', parsedState.faces.length/9, ' faces'); /* divide by 9 because 3 per face, 3 coords for vertx/uv/normal/whatever */
 
-  return processedLines;
+  return new Model( parsedState.verts, parsedState.uvs, parsedState.norms, parsedState.faces );
 }
